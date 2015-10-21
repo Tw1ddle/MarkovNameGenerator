2eb897b852246a2c4229d6b9440ec641381e47fa
sam <sam@silver42.karoo.co.uk>
2015-10-21 21:07:53 +0100

    Remove unused d3 and dat.gui externs

--- src/js/d3/event/Dispatch.hx
+++
@@ -1,7 +0,0 @@
-package js.d3.event;
-
-
-@:native("d3.dispatch")
-extern class Dispatch {
-  public function on(type:String, listener:Dynamic):Dispatch;
-}
--- src/js/d3/format/Format.hx
+++
@@ -1,7 +0,0 @@
-package js.d3.format;
-
-@:native("d3.format")
-extern class Format {
-	public var symbol : String;
-	public function scale(v:Float):Float;
-}
