410af59f3022ee4dd2481175b77455de578deae9
sam <sam@silver42.karoo.co.uk>
2016-01-17 07:52:37 +0000

    Implements way to load custom training data via the URL query string, use arrays instead of stringmap, alphabetically sort the training data preset list + minor refactoring

--- src/Main.hx
+++ src/Main.hx
@@ -1,6 +1,5 @@
 package;
 
-import haxe.ds.StringMap;
 import js.Browser;
 import js.html.Element;
 import js.html.InputElement;
@@ -18,60 +17,88 @@ using markov.util.StringExtensions;
 class Main {
 	private var generator:NameGenerator; // The Markov name generator
 	private var duplicateTrie:PrefixTrie; // Prefix trie for catching duplicates
-	private var trainingData:StringMap<Array<String>>; // The training data
+	private var trainingData:Array<TrainingData>; // The training data
 	/*
 	private var trieGraph:TrieForceGraph;
 	private var markovGraph:MarkovGraph;
 	*/
 	
-	private var trainingDataKey:String; // The selected training data key
-	private var numToGenerate:Int; // Number of names to try to generate
-	private var minLength:Int; // Minimum name length
-	private var maxLength:Int; // Maximum name length
-	private var order:Int; // Maximum order model that the name generator should use
-	private var prior:Float; // Value of the Dirichlet prior that the name generator should use
-	private var maxProcessingTime:Float; // Maximum time the name generator should spend generating a batch of names
-	private var startsWith:String; // String that names must start with
-	private var endsWith:String; // String that names must end with
-	private var includes:String; // String that names must include
-	private var excludes:String; // String that names must include
-	private var similar:String; // String that names are sorted by their similarity to
-	/*
-	private var generateTrieVisualization:Bool = false; // Generate a graph of the duplicate trie
-	private var generateMarkovVisualization:Bool = false; // Generate a graph of one of the markov models
-	private var markovVisualizationMinP:Float = 0.01; // Minimum p value required to draw one of the markov model edges
-	*/
-	
-	/*
-	 * Set the default values for name generation, filtering and sorting
-	 */
-	private inline function setDefaults():Void {
-		trainingDataKey = "animals";
-		numToGenerate = 100;
-		minLength = 5;
-		maxLength = 11;
-		order = 3;
-		prior = 0.005;
-		maxProcessingTime = 800;
-		startsWith = "";
-		startsWithElement.value = startsWith;
-		endsWith = "";
-		endsWithElement.value = endsWith;
-		includes = "";
-		includesElement.value = includes;
-		excludes = "";
-		excludesElement.value = excludes;
-		similar = "";
-		similarElement.value = similar;
-		
-		/*
-		markovVisualizationMinP = 0.01;
-		generateTrieVisualization = false;
-		generateMarkovVisualization = false;
-		*/
+    private static function main():Void {
+		var main = new Main();
 	}
 	
-	private var trainingDataElement:SelectElement;
+	private inline function new() {
+		// Builds a lookup table for the training data, matching the presets in the HTML
+		trainingData = new Array<TrainingData>();
+		
+		var addTrainingData = function(value:String, display:String, data:Array<String>):Void {
+			trainingData.push(new TrainingData(value, display, data));
+		}
+		
+		var addCustomTrainingData = function(value:String, display:String) {
+			trainingData.push(new TrainingData(value, "Custom", getCustomTrainingData()));
+		};
+		
+		addTrainingData("us_forenames", "American Forenames", FileReader.readFile("embed/usforenames.txt").split("\n"));
+		addTrainingData("tolkienesque_forenames", "Tolkienesque Forenames", FileReader.readFile("embed/tolkienesqueforenames.txt").split("\n"));
+		addTrainingData("werewolf_forenames", "Werewolf Forenames", FileReader.readFile("embed/werewolfforenames.txt").split("\n"));
+		addTrainingData("romandeity_forenames", "Roman Deity Forenames", FileReader.readFile("embed/romandeityforenames.txt").split("\n"));
+		addTrainingData("norsedeity_forenames", "Norse Deity Forenames", FileReader.readFile("embed/norsedeityforenames.txt").split("\n"));
+		addTrainingData("swedish_forenames", "Swedish Forenames", FileReader.readFile("embed/swedishforenames.txt").split("\n"));
+		addTrainingData("english_towns", "English Towns", FileReader.readFile("embed/englishtowns.txt").split("\n"));
+		addTrainingData("theological_demons", "Theological Demons", FileReader.readFile("embed/theologicaldemons.txt").split("\n"));
+		addTrainingData("scottish_surnames", "Scottish Surnames", FileReader.readFile("embed/scottishsurnames.txt").split("\n"));
+		addTrainingData("irish_forenames", "Irish Forenames", FileReader.readFile("embed/irishforenames.txt").split("\n"));
+		addTrainingData("icelandic_forenames", "Icelandic Forenames", FileReader.readFile("embed/icelandicforenames.txt").split("\n"));
+		addTrainingData("theological_angels", "Theological Angels", FileReader.readFile("embed/theologicalangels.txt").split("\n"));
+		addTrainingData("japanese_forenames", "Japanese Forenames", FileReader.readFile("embed/japaneseforenames.txt").split("\n"));
+		addTrainingData("french_forenames", "French Forenames", FileReader.readFile("embed/frenchforenames.txt").split("\n"));
+		addTrainingData("german_towns", "German Towns", FileReader.readFile("embed/germantowns.txt").split("\n"));
+		addTrainingData("animals", "Animals", FileReader.readFile("embed/animals.txt").split("\n"));
+		addTrainingData("pokemon", "Pokemon", FileReader.readFile("embed/pokemon.txt").split("\n"));
+		addTrainingData("fish", "Fish", FileReader.readFile("embed/fish.txt").split("\n"));
+		addTrainingData("plantscommon", "Plants (Common Names)", FileReader.readFile("embed/plantscommon.txt").split("\n"));
+		addTrainingData("countries", "Countries", FileReader.readFile("embed/countries.txt").split("\n"));
+		addTrainingData("clothing", "Clothing", FileReader.readFile("embed/clothing.txt").split("\n"));
+		//addTrainingData("profanity_filter", "Profanity", FileReader.readFile("embed/profanityfilter.txt").split("\n")); // Skipping this one for SEO and paranoia reasons
+		addCustomTrainingData("custom", "Custom");
+		
+		// Alphabetically sort the training data list items
+		trainingData.sort(function(a:TrainingData, b:TrainingData):Int {
+			var left = a.displayName.toLowerCase();
+			var right = b.displayName.toLowerCase();
+			if (left < right) {
+				return -1;
+			}
+			if (left > right) {
+				return 1;
+			}
+			return 0;
+		});
+		
+		// Generate the HTML training data selection list
+		var trainingDataList = cast Browser.document.getElementById("trainingdatalist");
+		for (data in trainingData) {
+			var option = Browser.document.createOptionElement();
+			option.appendChild(Browser.document.createTextNode(data.displayName));
+			option.value = data.value;
+			trainingDataList.appendChild(option);
+		}
+		
+		// Wait for the window to load before creating the sliders, listening for input etc
+		Browser.window.onload = onWindowLoaded;
+	}
+	
+	private inline function onWindowLoaded():Void {
+		getElementReferences();
+		setDefaults();
+		createSliders();
+		addEventListeners();
+		
+		trainingDataElementSelectionChanged();
+	}
+	
+	private var trainingDataListElement:SelectElement;
 	private var trainingDataTextEdit:InputElement;
 	private var orderElement:Element;
 	private var priorElement:Element;
@@ -94,8 +121,8 @@ class Main {
 	/*
 	 * Get references to the input elements on the webpage
 	 */
-	private inline function createElements():Void {
-		trainingDataElement = cast Browser.document.getElementById("trainingdatalist");
+	private inline function getElementReferences():Void {
+		trainingDataListElement = cast Browser.document.getElementById("trainingdatalist");
 		trainingDataTextEdit = cast Browser.document.getElementById("trainingdataedit");
 		orderElement = cast Browser.document.getElementById("order");		
 		priorElement = cast Browser.document.getElementById("prior");
@@ -116,47 +143,84 @@ class Main {
 		*/
 	}
 	
-    private static function main():Void {
-		var main = new Main();
+	/*
+	 * Try to take custom training data from "w" parameters in the URL
+	 */
+	private inline function getCustomTrainingData():Array<String> {
+		var params = Browser.window.location.search.substring(1);
+		
+		if (params == null || params == "") {
+			return [];
+		}
+		
+		var splitParams = params.split("&");
+		var trainingData = new Array<String>();
+		for (param in splitParams) {
+			var kv = param.split("=");
+			if (kv.length < 2) {
+				continue;
+			}
+			
+			if (kv[0] == "w") {
+				trainingData.push(kv[1]);
+			}
+		}
+		
+		return trainingData;
 	}
 	
-	private inline function new() {
-		// Build a lookup table for the training data
-		trainingData = new StringMap<Array<String>>();
-		trainingData.set("us_forenames", FileReader.readFile("embed/usforenames.txt").split("\n"));
-		trainingData.set("tolkienesque_forenames", FileReader.readFile("embed/tolkienesqueforenames.txt").split("\n"));
-		trainingData.set("werewolf_forenames", FileReader.readFile("embed/werewolfforenames.txt").split("\n"));
-		trainingData.set("romandeity_forenames", FileReader.readFile("embed/romandeityforenames.txt").split("\n"));
-		trainingData.set("norsedeity_forenames", FileReader.readFile("embed/norsedeityforenames.txt").split("\n"));
-		trainingData.set("swedish_forenames", FileReader.readFile("embed/swedishforenames.txt").split("\n"));
-		trainingData.set("english_towns", FileReader.readFile("embed/englishtowns.txt").split("\n"));
-		trainingData.set("theological_demons", FileReader.readFile("embed/theologicaldemons.txt").split("\n"));
-		trainingData.set("scottish_surnames", FileReader.readFile("embed/scottishsurnames.txt").split("\n"));
-		trainingData.set("irish_forenames", FileReader.readFile("embed/irishforenames.txt").split("\n"));
-		trainingData.set("icelandic_forenames", FileReader.readFile("embed/icelandicforenames.txt").split("\n"));
-		trainingData.set("theological_angels", FileReader.readFile("embed/theologicalangels.txt").split("\n"));
-		trainingData.set("japanese_forenames", FileReader.readFile("embed/japaneseforenames.txt").split("\n"));
-		trainingData.set("french_forenames", FileReader.readFile("embed/frenchforenames.txt").split("\n"));
-		trainingData.set("german_towns", FileReader.readFile("embed/germantowns.txt").split("\n"));
-		trainingData.set("animals", FileReader.readFile("embed/animals.txt").split("\n"));
-		trainingData.set("pokemon", FileReader.readFile("embed/pokemon.txt").split("\n"));
-		trainingData.set("fish", FileReader.readFile("embed/fish.txt").split("\n"));
-		trainingData.set("plantscommon", FileReader.readFile("embed/plantscommon.txt").split("\n"));
-		trainingData.set("countries", FileReader.readFile("embed/countries.txt").split("\n"));
-		trainingData.set("clothing", FileReader.readFile("embed/clothing.txt").split("\n"));
-		//trainingData.set("profanity_filter", FileReader.readFile("embed/profanityfilter.txt").split("\n")); // Skipping this one for SEO and paranoia reasons
-		
-		// Wait for the window to load before creating the sliders, listening for input etc
-		Browser.window.onload = onWindowLoaded;
-	}
+	private var trainingDataKey(default, set):String; // The selected training data key
+	private var numToGenerate:Int; // Number of names to try to generate
+	private var minLength:Int; // Minimum name length
+	private var maxLength:Int; // Maximum name length
+	private var order:Int; // Maximum order model that the name generator should use
+	private var prior:Float; // Value of the Dirichlet prior that the name generator should use
+	private var maxProcessingTime:Float; // Maximum time the name generator should spend generating a batch of names
+	private var startsWith:String; // String that names must start with
+	private var endsWith:String; // String that names must end with
+	private var includes:String; // String that names must include
+	private var excludes:String; // String that names must include
+	private var similar:String; // String that names are sorted by their similarity to
+	/*
+	private var generateTrieVisualization:Bool = false; // Generate a graph of the duplicate trie
+	private var generateMarkovVisualization:Bool = false; // Generate a graph of one of the markov models
+	private var markovVisualizationMinP:Float = 0.01; // Minimum p value required to draw one of the markov model edges
+	*/
 	
-	private inline function onWindowLoaded():Void {
-		createElements();
-		setDefaults();
-		createSliders();
-		addEventListeners();
+	/*
+	 * Set the default values for name generation, filtering and sorting
+	 */
+	private inline function setDefaults():Void {
+		// Optionally set custom data via a query string in the URL		
+		var data = findTrainingDataForKey("custom");
+		if (data != null && data.length > 0) {
+			trainingDataKey = "custom";
+		} else {
+			trainingDataKey = "animals";
+		}
 		
-		trainingDataElementSelectionChanged();
+		numToGenerate = 100;
+		minLength = 5;
+		maxLength = 11;
+		order = 3;
+		prior = 0.005;
+		maxProcessingTime = 800;
+		startsWith = "";
+		startsWithElement.value = startsWith;
+		endsWith = "";
+		endsWithElement.value = endsWith;
+		includes = "";
+		includesElement.value = includes;
+		excludes = "";
+		excludesElement.value = excludes;
+		similar = "";
+		similarElement.value = similar;
+		
+		/*
+		markovVisualizationMinP = 0.01;
+		generateTrieVisualization = false;
+		generateMarkovVisualization = false;
+		*/
 	}
 	
 	/*
@@ -316,7 +380,7 @@ class Main {
 	 * Add event listeners to the input elements, in order to update the values we feed the model when "generate" is pressed
 	 */ 
 	private inline function addEventListeners():Void {
-		trainingDataElement.addEventListener("change", function() {
+		trainingDataListElement.addEventListener("change", function() {
 			trainingDataElementSelectionChanged();
 		}, false);
 		
@@ -367,11 +431,11 @@ class Main {
 	}
 	
 	private function trainingDataElementSelectionChanged():Void {
-		if (trainingDataElement.value != null) {
-			trainingDataKey = trainingDataElement.value;
-			if (trainingData.get(trainingDataKey) != null) {
+		if (trainingDataListElement.value != null) {
+			trainingDataKey = trainingDataListElement.value;
+			var data = findTrainingDataForKey(trainingDataKey);
+			if (data != null) {
 				var s:String = "";
-				var data = trainingData.get(trainingDataKey);
 				for (i in data) {
 					s += i + " ";
 				}
@@ -443,7 +507,7 @@ class Main {
 	}
 	
 	/*
-	 * Helper method to set the names in the "names" section of the page
+	 * Helper method to set the generated names in the "names" section of the page
 	 */
 	private inline function setNames(names:Array<String>):Void {
 		if(similar.length > 0) {
@@ -472,5 +536,40 @@ class Main {
 			li.textContent = name.capitalize();
 			currentNamesElement.appendChild(li);
 		}
+	}
+	
+	/*
+	 * Helper method to search the training data array for a particular set of training data
+	 */
+	private function findTrainingDataForKey(key:String):Array<String> {
+		for (data in trainingData) {
+			if (data.value == key) {
+				return data.data;
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * Updates the selected preset item when the training data key is changed programatically
+	 */ 
+	private function set_trainingDataKey(key:String):String {
+		if (trainingDataListElement != null) {
+			trainingDataListElement.value = key;
+		}
+		return this.trainingDataKey = key;
+	}
+}
+
+// A set of name training data
+private class TrainingData {
+	public var value(default, null):String; // The "value" field in the select element
+	public var displayName(default, null):String; // The display name in the select element
+	public var data(default, null):Array<String>; // The training data itself
+	
+	public inline function new(value:String, displayName:String, data:Array<String>) {
+		this.value = value;
+		this.displayName = displayName;
+		this.data = data;
 	}
 }
