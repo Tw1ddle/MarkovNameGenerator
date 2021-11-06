3b12b5f2998779dbc0b774ecb7c76c77b7ba618f
sam <samcodesit@gmail.com>
2021-11-06 16:17:14 +0000

    Add shell script to generate dox

---
+++ lib/dox/generate_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This batch file generates the documentation for markov namegen.
+
+# Clean the generated documentation folder, to remove any old documentation.
+rm -rf "generated_docs"
+
+# Delete any existing generated XML-format type information.
+rm types.xml
+
+# Build the XML-format type information.
+haxe build.hxml
+
+# Generate the documentation.
+haxelib run dox -i types.xml -theme ./themes/samcodes --title "Markov Namegen API" -D version 1.0.7 --include "(markov)" -o generated_docs
