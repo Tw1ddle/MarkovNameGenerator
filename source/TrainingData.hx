12a873a35c0315aa3baa2966e6b72487ce4a8bb8
sam <samcodesit@gmail.com>
2021-12-14 23:37:13 +0000

    Move the training data to lib/word_lists in the library submodule

--- source/TrainingData.hx
+++ source/TrainingData.hx
@@ -3,6 +3,6 @@ package;
 import markov.util.TrainingDataBuilder;
 
 // Automatically reads training data from files into corresponding static arrays of strings in this class
-@:build(markov.util.TrainingDataBuilder.build("embed"))
+@:build(markov.util.TrainingDataBuilder.build("lib/word_lists"))
 @:keep
 class TrainingData {}
